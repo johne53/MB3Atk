625caf3c60b1b3598f6edaa0db3c51d8a2220aa5
John Emmas <johne53@tiscali.co.uk>
2014-05-03 15:58:21 +0100

    JOHNE - Bump the version number to 2.13.01

--- win32-fixup.pl
+++ win32-fixup.pl
@@ -3,13 +3,13 @@
 
 require "../local-paths.lib";
 
-$package_string = "\"atk 2.12.00\"";
-$package_version = "\"2.12.00\"";
-$atk_version = "2.12.00";
+$package_string = "\"atk 2.13.01\"";
+$package_version = "\"2.13.01\"";
+$atk_version = "2.13.01";
 $major = 2;
-$minor = 12;
-$micro = 0;
-$binary_age = 21200;
+$minor = 13;
+$micro = 1;
+$binary_age = 21301;
 $interface_age = 1;
 $gettext_package = "atk20";
 $current_minus_age = 0;
