dadd65740cde929b7ea975791ac58f9ba7c6d443
John Emmas <johne53@tiscali.co.uk>
2014-09-24 13:23:10 +0100

    JOHE - Add 'tests/test_atk.c' (which somehow got missed out earlier)

---
+++ tests/test_atk.c
@@ -0,0 +1,32 @@
+/* ATK -  Accessibility Toolkit
+ * Copyright 2001 Sun Microsystems Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include <atk/atk.h>
+
+int run_relation_tests  (gint argc, char* argv[]);
+int run_state_set_tests (gint argc, char* argv[]);
+
+int
+main (int argc, char* argv[])
+{
+	run_relation_tests(argc, argv);
+	run_state_set_tests(argc, argv);
+
+	return 0;
+}
